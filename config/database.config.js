ceef1d629178c5fe12e7ff588255dde0a6c7915c
venkatesh1702 <contactvenkatesh17@gmail.com>
2020-10-27 14:14:48 +0530

    commit venkatOct7

--- config/database.config.js
+++ config/database.config.js
@@ -1,6 +1,5 @@
-// module.exports = {
-//     url: 'mongodb+srv://venkat:venkat13750@cluster0.1pncw.mongodb.net/test?authSource=admin&replicaSet=atlas-67623l-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
-// }
 module.exports = {
-    url: 'mongodb+srv://venkat:demo1234@cluster0.6rmk0.mongodb.net/test'
+    // url: 'mongodb+srv://venkat:demo134@cluster0.6rmk0.mongodb.net/test'
+    url:'mongodb+srv://venkat:demo1234@cluster0.6rmk0.mongodb.net/test?retryWrites=true&w=majority'
 }
+
