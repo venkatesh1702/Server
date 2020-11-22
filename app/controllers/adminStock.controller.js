778e8b810447c1df55884017f61de08a533d3f24
Prasanth Sankar <prasanthsankar32@gmail.com>
2020-11-22 12:21:46 +0530

    stock changes

--- app/controllers/adminStock.controller.js
+++ app/controllers/adminStock.controller.js
@@ -2,10 +2,9 @@ const AdminStock = require('../models/adminStock.model');
 const multer = require('multer');
 
 
-exports.create = (req,res)=>{
+exports.save = (req,res)=>{
     
     const adminStock = new AdminStock({
-        
         stockName :req.body.stockName,
         marketCap :req.body.marketCap,
         peRatio :req.body.peRatio,
@@ -38,5 +37,104 @@ exports.create = (req,res)=>{
             message: err.message || "Error"
         });
     })
-}
+};
 
+exports.list = (req, res) => {
+    AdminStock.find()
+    .then(adminStock => {
+        res.send(adminStock);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Error"
+        });
+    });
+};
+
+exports.findOne = (req, res) => {
+    console.log(req.query.stockId)
+    AdminStock.findById(req.query.stockId)
+    .then(adminStock => {
+        if(!adminStock) {
+            return res.status(404).send({
+                message: "Note not found with id " + req.query.stockId
+            });            
+        }
+        res.send(adminStock);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.query.stockId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving note with id " + req.query.stockId
+        });
+    });
+};
+
+exports.update = (req, res) => {
+    console.log(req.query.stockId)
+    AdminStock.findOneAndUpdate({_id:req.query.stockId}, {
+        stockName :req.body.stockName,
+        marketCap :req.body.marketCap,
+        peRatio :req.body.peRatio,
+        pbRatio:req.body.pbRatio,
+        industryPE:req.body.industryPE,
+        divYield:req.body.divYield,
+        bookValue:req.body.bookValue,
+        epsTTM:req.body.epsTTM,
+        roe:req.body.roe,
+        aboutCompany:req.body.aboutCompany,
+        organization:req.body.organization,
+        foundedYear:req.body.foundedYear,
+        managingDirector:req.body.managingDirector,
+        shareHoldingPattern:req.body.shareHoldingPattern,
+        brandImg:req.body.brandImg,
+        sector:req.body.sector,
+        division:req.body.division,
+        address:req.body.address,
+        phone:req.body.phone,
+        launchDate:req.body.launchDate,
+        email:req.body.email,
+        website:req.body.website
+    },{new:true})
+    .then(adminStock => {
+        if(!adminStock) {
+            return res.status(404).send({
+                message: "Stock with this " + req.query.stockId
+            });
+        }
+        res.send(adminStock);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Stock found with id " + req.query.stockId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating Stock with id " + req.query.stockId
+        });
+    });
+};
+
+exports.delete = (req, res) => {
+    console.log(req.query.stockId)
+    AdminStock.findOneAndDelete({_id:req.query.stockId})
+    .then(adminStock => {
+        if(!adminStock) {
+            return res.status(404).send({
+                message: "Stock not found with id " + req.query.stockId
+            });
+        }
+        res.send({message: "Stock deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Stock not found with id " + req.query.stockId
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete Stock with id " + req.query.stockId
+        });
+    });
+};
--- app/routes/adminStock.routes.js
+++ app/routes/adminStock.routes.js
@@ -1,7 +1,9 @@
 module.exports = (app)=>{
    const adminStock = require('../controllers/adminStock.controller')
-   app.post('/api/adminStock',adminStock.create);
-   
-   
+   app.post('/api/adminStock/add',adminStock.save);
+   app.get('/api/adminStock/list',adminStock.list);
+   app.get('/admin/stock',adminStock.findOne);
+   app.put('/api/adminStock/update',adminStock.update);
+   app.delete('/api/adminStock/delete',adminStock.delete);
 }
 
